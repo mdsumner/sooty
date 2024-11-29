4078c266d6ebdb1b3d4e519fbcd2182c773a719b
Michael Sumner <mdsumner@gmail.com>
2024-11-30 02:02:53 +1100

    new AMSR

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# idt dev
+
+* Added 3.125km AMSR ice. 
+
 # idt 0.0.1
 
 * Initial package functionality. 
--- R/zzz.R
+++ R/zzz.R
@@ -13,6 +13,8 @@ utils::globalVariables(".data")
 
   Sys.setenv("AWS_NO_SIGN_REQUEST" = "YES")
   Sys.setenv("AWS_S3_ENDPOINT" = "projects.pawsey.org.au")
+  Sys.setenv("AWS_VIRTUAL_HOSTING" = "FALSE")
+
   Sys.setenv("GDAL_DISABLE_READDIR_ON_OPEN" = "EMPTY_DIR")
   Sys.setenv("VSI_CACHE"="TRUE")
   Sys.setenv("GDAL_NETCDF_ASSUME_LONGLAT" = "YES")
