1a0720481c98757e0a471a158b92c95b8944f904
Michael Sumner <mdsumner@gmail.com>
2025-02-12 20:55:13 +1100

    cleanup with S7 a bit

--- R/dataset.R
+++ R/dataset.R
@@ -7,6 +7,16 @@ NULL
 #' Generates an object whose "@id" property may be set, which then communicates with a dataset of files/objects that
 #' sooty knows about.
 #'
+#' Compare 'curated' to 'sooty_files(curated = FALSE)', if its curated sooty knows what dataset it belongs to
+#'
+#' @param id a dataset label, see 'dataset()@available_datasets' (get, and settable)
+#' @param n the number of files (objects) comprising the dataset (get, not settable)
+#' @param mindate the minimum available date for the files
+#' @param maxdate the maximum available date for the files
+#' @param available_datasets the names of available datasets, for 'id' (get, not settable)
+#' @param curated the full set of files (objects) that sooty knows about that belong to datasets (get, not settable)
+#' @param source the set of files (objects) belonging to this dataset (get, not settable)
+#' @usage NULL
 #' @importFrom S7 new_class new_property class_character class_integer class_POSIXct class_data.frame
 #' @importFrom dplyr filter
 #' @export
